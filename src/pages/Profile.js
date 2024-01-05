af97cb871a3b1f5462a011e1e8ade5eed3bda12f
Inês Torres <inestsilva2001@hotmail.com>
2024-01-05 18:53:19 +0000

    finalizacao do componente modal

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Modal from '../components/Modal'
 
 const Profile = () => {
+    const [fecharModal, setFecharModal] = useState(true);
     return (
         <div>
-            <Modal message="Texto da modal"/>
+            <button onClick={() => {
+                setFecharModal(!fecharModal);
+            }}>Abrir modal</button>
+            <Modal
+                fecharModal={fecharModal}
+                setFecharModal={setFecharModal}
+                message="Texto da modal - exemplo"
+            />
         </div>
     )
 }
