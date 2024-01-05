68857793110ad7349bf52713d778672927491a56
Inês Torres <inestsilva2001@hotmail.com>
2024-01-05 20:19:27 +0000

    criacao do componente botao, adicionar comentarios, atualizar pesos das letras da modal

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,19 +1,28 @@
 import React, { useState } from 'react'
 import Modal from '../components/Modal'
+import Button from '../components/Button';
 
+//Pagina do perfil
 const Profile = () => {
     const [fecharModal, setFecharModal] = useState(true);
     return (
-        <div>
+        <>
             <button onClick={() => {
                 setFecharModal(!fecharModal);
             }}>Abrir modal</button>
+            <Button
+                onClick={() => {
+                    setFecharModal(!fecharModal);
+                }}
+                text={"Próximo"}
+                disable={false}
+            />
             <Modal
                 fecharModal={fecharModal}
                 setFecharModal={setFecharModal}
                 message="Texto da modal - exemplo"
             />
-        </div>
+        </>
     )
 }
 
