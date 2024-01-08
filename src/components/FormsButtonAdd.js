b35cc997344487ba10bcd63bd6ef57b8756e5a68
cerkeira <ricardocerqueira0@protonmail.com>
2024-01-08 04:56:12 +0000

    Botões de Formulário

---
+++ src/components/FormsButtonAdd.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const FormsButtonAdd = () => {
+  return (
+    <div>
+      <button
+        className="FormsButton"
+      >
+        <div className="FormsButtonOrg">
+          <p>Transportadora Eco-Friendly</p> <p className="adder">+</p>
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default FormsButtonAdd;
