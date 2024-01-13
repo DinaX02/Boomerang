b1c64ed3aa236373e11432ac641a9dcce6a1290d
DinaX <dinisantunes@ua.pt>
2024-01-13 23:06:57 +0000

    etapa 3 progresso publicar-concluída -bottom sheet c/ drag feito

--- src/components/ProgressPublish/ProgressPublish3.js
+++ src/components/ProgressPublish/ProgressPublish3.js
@@ -1,13 +1,14 @@
-import React, {useState} from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Button from "../Button";
 import InputMeasuresNotMandatory from "../InputsMesuresNotMandatory";
 import ButtonWithInfoIcon from "../ButtonWithInfoIcon";
 import Header from "../Header/Header";
-
-import "../components.css";
-
+import BottomSheet from "../BottomSheetSizes";
+import Draggable from "react-draggable";
+import { useDispatch, useSelector } from "react-redux";
+import { updateProgressPublish1 } from "../../redux/publicarSlice";
 
 const SpaceTopComponent = styled.div`
   margin-top: 2.5em;
@@ -28,8 +29,22 @@ const ContainerDoisBtn = styled.div`
 `;
 
 const ProgressPublish3 = () => {
-
+  const [bottomSheetOpen, setBottomSheetOpen] = useState(false);
+  const bottomSheetRef = useRef(null);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const selectedOption = useSelector(
+    (state) => state.Publicar1.progressPublish1.conditionOfClothing
+  );
+
+  // console.log("opçao seleciona foi2....", selectedOption);
+
+  const [nextButtonDisabled, setNextButtonDisabled] = useState(!selectedOption);
+
+  const handleToggleBottomSheet = () => {
+    setBottomSheetOpen(!bottomSheetOpen);
+  };
 
   const handleGoBackStepPublish = () => {
     navigate(-1);
@@ -39,18 +54,47 @@ const ProgressPublish3 = () => {
     navigate("/progressPublish-4");
   };
 
+  const handleOptionSelect = (option) => {
+    dispatch(updateProgressPublish1({ conditionOfClothing: option }));
+    setBottomSheetOpen(false);
+    setNextButtonDisabled(false);
+  };
+
+  useEffect(() => {
+    // Atualize nextButtonDisabled quando selectedOption mudar
+    setNextButtonDisabled(!selectedOption);
+  }, [selectedOption]);
+
   return (
     <div>
       <Header name="Publicar / Etapa 3 de 5" />
+      {bottomSheetOpen && (
+        <Draggable
+          cancel=".no-drag"
+          bounds="parent"
+          positionOffset={{ x: "0", y: "0" }}
+          onStop={() => setBottomSheetOpen(false)}
+        >
+          <BottomSheet
+            ref={bottomSheetRef}
+            onClose={() => setBottomSheetOpen(false)}
+            onSelectOption={handleOptionSelect}
+          />
+        </Draggable>
+      )}
       <SpaceTopComponent>
-        <ButtonWithInfoIcon btnName="Selecionar Estado" infoName="Estado"/>
+        <ButtonWithInfoIcon
+          infoName="Estado"
+          onClick={handleToggleBottomSheet}
+          selectedOption={selectedOption}
+        />
       </SpaceTopComponent>
-      <InputMeasuresNotMandatory/>
+      <InputMeasuresNotMandatory />
       <ContainerDoisBtn>
         <Button text="Anterior" onClick={handleGoBackStepPublish} />
-        <Button text="Próximo" onClick={handleNextStepPublish} />
+        <Button text="Próximo" onClick={handleNextStepPublish} disable={nextButtonDisabled} />
       </ContainerDoisBtn>
-  </div>
+    </div>
   );
 };
 
