cb335a1ce1929c6305e1be5f6d95bd518935bf54
cerkeira <ricardocerqueira0@protonmail.com>
2024-01-14 03:07:20 +0000

    Componente notifcacao terminado e acerto no tamanho dos icones do menu

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,52 +1,79 @@
-import React from 'react'
-import vestidopreto from '../assets/vestido_preto_1.png'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 const Notification = (props) => {
 
-  const Notification = styled.div`
-  border-radius: 5px;
-  background: #fff;
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.09);
-  height: 130px;
-  width: 90%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  p{
-    max-width: 250px;
-    margin-bottom: 5px;
-  }
-  `
+  const [maxDescriptionLength, setMaxDescriptionLength] = useState(90);
 
-  const NotificationImg =styled.div`
-  width: 80px;
-  height: 80px;
-  border-radius: 200px;
-  margin-right: 20px;
-  margin-left: 20px;
-  background-size: cover;
-  background-position: center center;
-  background: ${props.discount ? '#00C17C' : `url(${vestidopreto})`};
-  font-size: 20px;
-  color: white;
-  font-weight: 600;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  `
+  const handleResize = () => {
 
+    if (window.innerWidth <= 450) {
+      setMaxDescriptionLength(25);
+    } else if (window.innerWidth <= 800) {
+      setMaxDescriptionLength(50);
+    } else{
+      setMaxDescriptionLength(90);
+    }
+  };
+
+  useEffect(() => {
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const descriptionSizeControl = (text) => {
+    return text.length > maxDescriptionLength
+      ? `${text.substring(0, maxDescriptionLength)}...`
+      : text;
+  };
 
   return (
-    <Notification>
-      <NotificationImg alt='notificação'>{props.discount}</NotificationImg>
+    <NotificationDiv>
+      <NotificationImg alt='notificação' style={{ background: props.image ? `url(${props.image})` : '#00C17C' }}>{props.discount}</NotificationImg>
       <div>
-      <p><b>{props.title}</b></p>
-      <p>{props.sub}</p>
+      <p><b>{descriptionSizeControl(props.title)}</b></p>
+      <p>{descriptionSizeControl(props.sub)}</p>
       </div>
        
-    </Notification> 
+    </NotificationDiv> 
   )
 }
+
+const NotificationDiv = styled.div`
+border-radius: 5px;
+background: #fff;
+box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.09);
+height: 110px;
+width: 90%;
+display: flex;
+flex-direction: row;
+align-items: center;
+p{
+  margin-bottom: 5px;
+}
+`
+
+const NotificationImg =styled.div`
+width: 80px;
+height: 80px;
+border-radius: 500px;
+margin-right: 20px;
+margin-left: 20px;
+background-size: cover !important;
+background-position: center center;
+font-size: 20px;
+color: white;
+font-weight: 600;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
 
 export default Notification
