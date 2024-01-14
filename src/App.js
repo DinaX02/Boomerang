4ab7a9a766c63611d1db5cd6e1cfafb141068d68
margaridalvesmm <mariapereiraa291@gmail.com>
2024-01-14 00:33:48 +0000

    Adicionar morada

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import AboutPage from "./pages/AboutPage";
 import '../src/components/components.css';
 import ProgressPublish2 from "./components/ProgressPublish/ProgressPublish2.js";
 import Notifications from "./pages/Notifications";
+import AlugarMorada from "./components/AlugarMorada";
+import AdicionarMorada from "./components/AdicionarMorada";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
         <Route path="/publicar-page" element={<Publicar/>}></Route>
         <Route path="/profile-page" element={<Profile/>}></Route>
         <Route path="/progressPublish-2" element={<ProgressPublish2/>}></Route>
+        <Route path="/alugar-morada" element={<AlugarMorada/>}></Route>
+        <Route path="/adicionar-morada" element={<AdicionarMorada/>}></Route>
         <Route path="/about-page" element={<AboutPage/>}></Route>
         <Route path="/notifications-page" element={<Notifications/>}></Route>
       </Routes>
