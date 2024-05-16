17c4303b359cbc011593fc5d9cad74d60ab8da6f
DinaX <dinisantunes@ua.pt>
2024-05-16 22:27:28 +0100

    integração da pesquisa por users com consumo à api e perfil dos users (endpoints- seeUser e searchUser)

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,15 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import PublishSlice from "./publicarSlice";
 import RentSlice from "./rentSlice";
 import { myAPI } from "./productAPI";
+import myUserAPI from "./usersAPI";
 
 const store = configureStore({
   reducer: {
     Publicar1: PublishSlice,
     Rent: RentSlice,
     [myAPI.reducerPath]: myAPI.reducer,
+    [myUserAPI.reducerPath]: myUserAPI.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(myAPI.middleware),
+    getDefaultMiddleware().concat(myAPI.middleware, myUserAPI.middleware),
 });
 
 export default store;
