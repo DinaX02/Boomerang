da2c2cb60c6d6fac2f3a12b648c3bb6e7a9fe3e0
Inês Torres <inestsilva2001@hotmail.com>
2024-06-19 00:44:02 +0100

    measurements

--- src/redux/publicarSlice.js
+++ src/redux/publicarSlice.js
@@ -6,50 +6,43 @@ const PublishSlice = createSlice({
     progressPublish1: {
       title: "",
       description: "",
-      measurements: "",
+      measurements: {}, // Inicializado como objeto
       value: 0,
       price_day: 0,
       brand: "",
       SizeId: 0,
-      ProductTypeId:0,
+      ProductTypeId: 0,
       ColorId: 0,
       GradeId: 0,
       imageUrls: [],
       countChar: 0,
-      // measureBusto: "",
-      // measureCintura: "",
-      // measureQuadril: "",
-      // measureComprimento: "",
     },
-    
   },
   reducers: {
     updateProgressPublish1: (state, action) => {
       state.progressPublish1 = { ...state.progressPublish1, ...action.payload };
     },
+    updateMeasurements: (state, action) => {
+      state.progressPublish1.measurements = { ...state.progressPublish1.measurements, ...action.payload };
+    },
     resetProgressPublish1: (state) => {
-      // limpar os dados do pulicar
       state.progressPublish1 = {
-      title: "",
-      description: "",
-      measurements: "",
-      value: 0,
-      price_day: 0,
-      brand: "",
-      SizeId: 0,
-      ProductTypeId:0,
-      ColorId: 0,
-      GradeId: 0,
-      imageUrls: [],
-      countChar: 0,
-      // measureBusto: "",
-      // measureCintura: "",
-      // measureQuadril: "",
-      // measureComprimento: "",
+        title: "",
+        description: "",
+        measurements: {}, // Reiniciar measurements como um objeto vazio
+        value: 0,
+        price_day: 0,
+        brand: "",
+        SizeId: 0,
+        ProductTypeId: 0,
+        ColorId: 0,
+        GradeId: 0,
+        imageUrls: [],
+        countChar: 0,
       };
     },
   },
 });
 
-export const { updateProgressPublish1, resetProgressPublish1} = PublishSlice.actions;
+export const { updateProgressPublish1, updateMeasurements, resetProgressPublish1 } = PublishSlice.actions;
 export default PublishSlice.reducer;
