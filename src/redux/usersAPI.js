026ae477fd217b00def1b26105d1564814e07fdd
cerkeira <ricardocerqueira0@protonmail.com>
2024-06-24 06:56:34 +0100

    localhost ready

--- src/redux/usersAPI.js
+++ src/redux/usersAPI.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const myUserAPI = createApi({
   reducerPath: "userAPI",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "http://localhost:3000/",
-    baseUrl: "https://boomerang-4hhtobs79-boomerangs-projects.vercel.app/",
+    baseUrl: "http://localhost:3000/",
+    // baseUrl: "https://boomerang-4hhtobs79-boomerangs-projects.vercel.app/",
     prepareHeaders: (headers) => {
       headers.set("Content-Type", "application/json");
       return headers;
